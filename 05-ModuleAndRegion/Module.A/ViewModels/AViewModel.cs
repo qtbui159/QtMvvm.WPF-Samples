d2232ecd5c2501f7b5485028e13932b2d15e1913
qidong5566 <635877990@qq.com>
2019-03-29 17:00:45 +0800

    MoudleAndRegion

---
+++ 05-ModuleAndRegion/Module.A/ViewModels/AViewModel.cs
@@ -0,0 +1,30 @@
+﻿using QtMvvm.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.A.ViewModels
+{
+   public class AViewModel:ViewModelBase
+    {
+        private string m_Title;
+        public string Title
+        {
+            get
+            {
+                return m_Title;
+            }
+            set
+            {
+                SetProperty(ref m_Title, value);
+            }
+        }
+
+        public AViewModel()
+        {
+            Title = "ViewA";
+        }
+    }
+}
---
+++ 05-ModuleAndRegion/ModuleAndRegion/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Module.A.Views;
+using QtMvvm.WPF.Command;
+using QtMvvm.WPF.Module;
+using QtMvvm.WPF.Region;
+using QtMvvm.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleAndRegion
+{
+   public class MainWindowViewModel:ViewModelBase
+    {
+        public RelayCommand<string> NavigateCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            NavigateCommand = new RelayCommand<string>(Navigate);
+        }
+
+        private void Navigate(string obj)
+        {
+            if (obj != null)
+            {
+                RegionManager.Navigate("myRegion", nameof(AView));
+            }
+        }
+    }
+}
