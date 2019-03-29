ad771374d1b24665436b39d6e3cb86c715e35945
qtbui159 <qtbui159@live.com>
2019-03-29 13:10:51 +0800

    [+]例子4

---
+++ 4.EventToCommand/EventToCommand/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using QtMvvm.WPF.Command;
+using QtMvvm.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EventToCommand
+{
+    class MainWindowViewModel:ViewModelBase
+    {
+        public string Position
+        {
+            private set => SetProperty(ref m_Position, value);
+            get => m_Position;
+        }
+        private string m_Position = "";
+
+        public RelayCommand<MouseEventArgs> MouseMoveCommand { private set; get; }
+
+        public MainWindowViewModel()
+        {
+            MouseMoveCommand = new RelayCommand<MouseEventArgs>(MouseMoveCommandProc);
+        }
+
+        private void MouseMoveCommandProc(MouseEventArgs e)
+        {
+            var position = e.MouseDevice.GetPosition(e.Device.Target);
+            Position = $"X:{position.X}  Y:{position.Y}";
+        }
+    }
+}
